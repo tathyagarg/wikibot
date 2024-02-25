361772218e1b6f8a534fd5d446688602bd89ffb9
Tathya <94824143+tathyagarg@users.noreply.github.com>
2024-02-25 18:29:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ with open("corpus.txt", "r") as f:
     corpus = [line.strip() for line in f.readlines()]
 
 token_maker = tokens.Tokenizer(corpus)
-words = token_maker.tokenize_word_sent(False)
-print(lemma.Lemmatizer(words).tag_part_of_speech(0))
+words = token_maker.tokenize_word_sent(punctuation=False)
+broken = token_maker.break_contractions(words)
+print(f"{broken=}")
+print(lemma.Lemmatizer(broken).tag_part_of_speech())
