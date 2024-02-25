ad85b5dcb32d1465e902395b4e64c5dd1b9c2444
Tathya <94824143+tathyagarg@users.noreply.github.com>
2024-02-25 18:29:34 +0530

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -4,34 +4,54 @@ def purify(text: list[str]) -> list[str]:
     return [i.strip() for i in text if i.strip()]
 
 class Tokenizer:
+    CONTRACTIONS = {
+        "can't": ["can", "not"],
+        "won't": ["will", "not"],
+        "wouldn't": ["would", "not"],
+        "shouldn't": ["should", "not"]
+    }
     def __init__(self, text: str | list[str]) -> None:
         self.text: str = text
-        self.is_list = isinstance(text, list)
-        self.action_text = [self.text, " ".join(self.text)][self.is_list]
+        
+    def tokenize_word(self, punctuation: bool = True, sentence_idx: int = 0) -> list[str]:
+        pattern: str = r'\s+' if punctuation else r"[^\w']+"
 
-    def tokenize_sent(self) -> list[str]:
-        if self.is_list: return self.text
-        result: list[str] = re.split(r'(?<=[.!?])', self.text)
-        result = [sent.strip() for sent in result if sent.strip()]
-        return result
-    
-    def tokenize_word(self, punctuation: bool = True) -> list[str]:
-        pattern: str = r'\s+' if punctuation else r'\W+'
-
-        result: list[str] = re.split(pattern, self.action_text)
+        result: list[str] = re.split(pattern, self.text[sentence_idx])
         return purify(result)
 
     def tokenize_word_sent(self, punctuation: bool = True) -> list[list[str]]:
-        result: list[str] = self.tokenize_sent()
-        result = [Tokenizer(sent).tokenize_word(punctuation) for sent in result]
+        result = [self.tokenize_word(punctuation, i) for i in range(len(self.text))]
         return result
     
     def tokenize_punc_sent(self) -> list[list[str]]:
-        result: list[str] = self.tokenize_sent()
+        result = self.text[:]
         for (index, sent) in enumerate(result):
             result[index] = purify(re.split(r'(\W)', sent))
         return result
 
-    def tokenize_punc(self) -> list[str]:
-        result: list[str] = re.split(r'(\W)', self.action_text)
-        return purify(result)
+    def break_contractions(self, tokenized) -> list[str]:
+        result: list[list[str]] = []
+        for sentence in tokenized:
+            result.append([])
+            for word in sentence:
+                if not (
+                    word.endswith("'m") or \
+                    word.endswith("'re") or \
+                    word.endswith("n't") or \
+                    word.endswith("'s")
+                ):
+                    result[-1].append(word)
+
+                if word.endswith("'m"):
+                    result[-1].extend(["I", "am"])
+                if word.endswith("'re"):
+                    result[-1].extend([word[:-3], "are"])
+                if word.endswith("n't"):
+                    capitalize = word[0].isupper()
+                    broken = Tokenizer.CONTRACTIONS[word]
+                    broken[0] = broken[0].capitalize() if capitalize else broken[0].lower()
+                    result[-1].extend(broken)
+                if word.endswith("'s"):
+                    result[-1].extend([word[:-2], "is"])
+                
+        return result
